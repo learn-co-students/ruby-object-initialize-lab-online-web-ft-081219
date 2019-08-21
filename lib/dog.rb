b183af7d642df8d71d0076a932409e6249c89bdd
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-21 01:45:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog 
+  def initialize(name, breed="Mutt")
+    @name = name 
+    @breed = breed
+  end 
+  
+  def name=(name)
+    @dog_name = name
+  end
+  def breed=(breed)
+    @dog_breed = breed 
+  end
+end
